06462d3ea4cd7f27bd65140897dfbb6351d232be
2fps <echoweb@126.com>
2020-01-18 21:30:58 +0800

    功能新增

--- react/webpack.config.base.js
+++ react/webpack.config.base.js
@@ -1,138 +1,166 @@
-const path = require('path');
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const HappyPack = require('happypack')
+const os = require('os')
+
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        chunkFilename: 'js/[name].[hash:7].js',
-        filename: 'js/[name].[hash:7].js',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    chunkFilename: 'js/[name].[hash:7].js',
+    filename: 'js/[name].[hash:7].js',
+  },
+  resolve: {
+    // 设置别名
+    alias: {
+      '@': path.resolve('src')
     },
-    resolve: {
-        // 设置别名
-        alias: {
-            '@': path.resolve('src')
-        },
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-        },
-        runtimeChunk: true
-    },
-    module: {
-        rules: [
-            /* {
-                test: /\.(js|jsx|ts|tsx)$/,
-                enforce: 'pre',
-                use: {
-                    loader: 'eslint-loader',
-                    options: {
-                        emitWarning: true,
-                        // emitError: true,
-                        cache: true,
-                        formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
-                    }
-                },
-                exclude: /node_modules/
-            }, */ {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'babel-loader'
-                }],
-            }, {
-                test: /\.(ts|tsx)$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'ts-loader'
-                }],
-            }, {
-              test: /\.(png|jpeg|jpg|gif|svg)$/,
-              loader: 'url-loader',
-              options: {
-                  limit: 8192,
-                  name: path.posix.join('static', 'img/[name].[hash:7].[ext]')
-               }
-            }, { // 字体图标
-                test: /\.(eot|woff|woff2|ttf)$/i,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192,
-                    }
-                }
-            }, {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins:[
-                            require('autoprefixer')
-                        ]
-                    }
-                }]
-            }, {
-                test: /\.less$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins:[
-                            require('autoprefixer')
-                        ]
-                    }
-                }, {
-                    loader: 'less-loader'
-                }]
-            }, {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader'
-                }, {
-                    loader: 'postcss-loader',
-                    options: {
-                        plugins:[
-                            require('autoprefixer')
-                        ]
-                    }
-                }, {
-                    loader: 'sass-loader'
-                }]
+    runtimeChunk: true
+  },
+  stats: {
+    assets: false,
+    builtAt: false,
+    modules: false,
+    entrypoints: false
+  },
+  module: {
+    // noParse: function(content) {
+    //   return /react/.test(content)
+    // },
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+          options: {
+            emitWarning: true,
+            // emitError: true,
+            cache: true,
+            formatter: require('eslint-friendly-formatter') // 指定错误报告的格式规范
+          }
+        }
+      }, {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+        use: 'happypack/loader?id=babel',
+        // use: [{
+        //     loader: 'babel-loader'
+        // }],
+      }, /*  {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [{
+              loader: 'babel-loader'
+          }],
+      }, */ {
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
+        loader: 'url-loader',
+        include: [path.resolve('src')],
+        options: {
+          limit: 8192,
+          name: path.posix.join('static', 'img/[name].[hash:7].[ext]')
+        }
+      }, { // 字体图标
+        test: /\.(eot|woff|woff2|ttf)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192,
+          }
+        }
+      }, {
+        test: /\.css$/i,
+        use: [
+          // {loader: 'style-loader'},
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              plugins:[
+                require('autoprefixer')
+              ]
             }
-        ]
-    },
-    plugins: [
-        // new webpack.HotModuleReplacementPlugin()
-        // new BundleAnalyzerPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[hash:7].css',
-            ignoreOrder: true
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'src', 'index.html'),//模板
-            filename: 'index.html',
-            inject: true, //允许插件修改哪些内容，包括head与body
-            hash: true, //是否添加hash值
-            minify: { //压缩HTML文件
-                removeComments: true,//移除HTML中的注释
-                collapseWhitespace: true //删除空白符与换行符
-            },
-            chunksSortMode: 'none' //如果使用webpack4将该配置项设置为'none'
-        }),
+          }]
+      }, {
+        test: /\.less$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [
+                require('autoprefixer')
+              ]
+            }
+          }, {
+            loader: 'less-loader'
+          }]
+      }, {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              plugins:[
+                require('autoprefixer')
+              ]
+            }
+          }, {
+            loader: 'sass-loader'
+          }]
+      }
     ]
-};
+  },
+  plugins: [
+    // new webpack.HotModuleReplacementPlugin()
+    // new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash:7].css',
+      ignoreOrder: true
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),//模板
+      filename: 'index.html',
+      inject: true, // 允许插件修改哪些内容，包括head与body
+      hash: true, // 是否添加hash值
+      minify: { // 压缩HTML文件
+        removeComments: true, // 移除HTML中的注释
+        collapseWhitespace: true // 删除空白符与换行符
+      },
+      chunksSortMode: 'none' // 如果使用webpack4将该配置项设置为'none'
+    }),
+    new HappyPack({ // 基础参数设置
+      id: 'babel', // 上面loader?后面指定的id
+      loaders: ['babel-loader?cacheDirectory'], // 实际匹配处理的loader
+      threadPool: happyThreadPool,
+      // cache: true // 已被弃用
+      verbose: true
+    })
+    // new webpack.DllReferencePlugin({
+    //     manifest: require('./dist/react.manifest.json')
+    // })
+  ]
+}
 
--- react/webpack.config.prod.js
+++ react/webpack.config.prod.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 const baseWebpackConfig = require('./webpack.config.base');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const FileManagerPlugin = require('filemanager-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const plugins = [
     new CleanWebpackPlugin()
@@ -27,6 +29,40 @@ if (process.env.ZIP) {
 
 module.exports = merge(baseWebpackConfig, {
     mode: 'production',
+    // optimization: {
+    //     minimizer: [
+    //         new UglifyJSPlugin({
+    //             uglifyOptions: {
+    //                 compress: {
+    //                     drop_console: true,
+    //                 }
+    //             }
+    //         })
+    //     ]
+    // },
+    // optimization: {
+    //     minimizer: [
+    //         new TerserPlugin({
+    //             minify: (file, sourceMap) => {
+    //                 // https://github.com/mishoo/UglifyJS2#minify-options
+    //                 const uglifyJsOptions = {
+    //                     /* your `uglify-js` package options */
+    //                     compress: {
+    //                         drop_console: true
+    //                     }
+    //                 };
+            
+    //                 if (sourceMap) {
+    //                     uglifyJsOptions.sourceMap = {
+    //                         content: sourceMap,
+    //                     };
+    //                 }
+            
+    //                 return require('uglify-js').minify(file, uglifyJsOptions);
+    //             },
+    //         }),
+    //     ],
+    // },
     devtool: 'source-map',
     plugins
 });
